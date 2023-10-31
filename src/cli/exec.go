504aa75464a88c5f4920ecd3ed4d24413c3b8ae9
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-10-31 07:42:29 +0000

    Add helper to run cmd & return output

--- src/cli/exec.go
+++ src/cli/exec.go
@@ -6,3 +6,8 @@ func RunCmd(cmd string, args ...string) error {
 	subProcess := exec.Command(cmd, args...)
 	return subProcess.Run()
 }
+
+func RunCmdWithOutput(cmd string, args ...string) ([]byte, error) {
+	subProcess := exec.Command(cmd, args...)
+	return subProcess.Output()
+}
