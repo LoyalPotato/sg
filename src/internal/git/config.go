3ca47cf03b629c8724618c4570173bb8597d82ed
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-03 08:42:04 +0000

    Add docs & update imports

--- src/internal/git/config.go
+++ src/internal/git/config.go
@@ -3,7 +3,7 @@ package git
 import (
 	"fmt"
 
-	"github.com/LoyalPotato/sg/src/cli"
+	"github.com/LoyalPotato/sg/src/internal/cli"
 	"github.com/LoyalPotato/sg/src/internal/styles"
 	"github.com/LoyalPotato/sg/src/internal/utils"
 )
@@ -13,6 +13,7 @@ type GitConfig struct {
 	LogRun     bool
 }
 
+// Add item to local git config
 func AddConfig(gitConfig GitConfig) error {
 	args := []string{"config", "--add", gitConfig.Key, gitConfig.Value}
 	if gitConfig.LogRun {
@@ -22,18 +23,21 @@ func AddConfig(gitConfig GitConfig) error {
 	return cli.RunCmd("git", args...)
 }
 
+// Get local single git config
 func GetConfig(key string) (string, error) {
 	out, err := cli.RunCmdWithOutput("git", "config", "--get", key)
 
 	return string(out), err
 }
 
+// Check if local git config exists
 func ConfigExists(key string) bool {
 	_, err := cli.RunCmdWithOutput("git", "config", "--get", key)
 
 	return utils.GetExitCode(err) != 1
 }
 
+// Delete local git config
 func UnsetConfig(key string) error {
 	return cli.RunCmd("git", "config", "--unset-all", key)
 }
--- src/internal/git/validation.go
+++ src/internal/git/validation.go
@@ -1,9 +1,10 @@
 package git
 
 import (
-	"github.com/LoyalPotato/sg/src/cli"
+	"github.com/LoyalPotato/sg/src/internal/cli"
 )
 
+// Checks if current folder is a git repository
 func IsGitRepo() error {
 	return cli.RunCmd("git", "rev-parse", "--git-dir")
 }
