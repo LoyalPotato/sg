ca764d712095768832316034973efe6b6b2c335d
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-03 08:41:11 +0000

    Add git town main branch config

---
+++ src/internal/git-town/config.go
@@ -0,0 +1,25 @@
+package gittown
+
+import (
+	"github.com/LoyalPotato/sg/src/internal/git"
+)
+
+const MainBranchKey = "git-town.main-branch-name"
+
+func AddMainBranchConfig(brachName string, logRun bool) error {
+	return git.AddConfig(git.GitConfig{
+		Key:    MainBranchKey,
+		Value:  brachName,
+		LogRun: logRun,
+	})
+}
+
+func RemoveMainBranchConfig() (bool, error) {
+	exists := git.ConfigExists(MainBranchKey)
+
+	if !exists {
+		return exists, nil
+	}
+
+	return exists, git.UnsetConfig(MainBranchKey)
+}
