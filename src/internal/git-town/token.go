04de16cf17edf6051478c2a62734abdcd55b246a
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-03 08:42:46 +0000

    Rename token management file

--- src/internal/git-town/token.go
+++ src/internal/git-town/token.go
@@ -4,22 +4,22 @@ import (
 	"github.com/LoyalPotato/sg/src/internal/git"
 )
 
-const tokenKey = "git-town.github-token"
+const TokenKey = "git-town.github-token"
 
 func AddGithubToken(token string, logRun bool) error {
 	return git.AddConfig(git.GitConfig{
-		Key:    tokenKey,
+		Key:    TokenKey,
 		Value:  token,
 		LogRun: logRun,
 	})
 }
 
 func RemoveGithubToken() (bool, error) {
-	exists := git.ConfigExists(tokenKey)
+	exists := git.ConfigExists(TokenKey)
 
 	if !exists {
 		return exists, nil
 	}
 
-	return exists, git.UnsetConfig(tokenKey)
+	return exists, git.UnsetConfig(TokenKey)
 }
