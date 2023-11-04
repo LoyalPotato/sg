9d2a97291d64150d93d4fd08cf7e84f235cc902f
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-04 18:32:28 +0000

    Add rest of messages

--- src/internal/messages/en.go
+++ src/internal/messages/en.go
@@ -10,6 +10,7 @@ const (
 	GitTown_Hack   = "git-town hack %s\n"
 	GitTown_Append = "git-town append %s\n"
 	GitTown_Ship   = "git-town ship %s\n"
+	GitTown_Sync   = "git-town sync"
 
 	No_Changes = "It seems like there are no new changes... Please make a change to be able to proceed!"
 
@@ -35,13 +36,15 @@ We have a new feature that we need to develop. But there are some other things t
 - Implement the feature
 	
 We should clean up first, to have a good starting base!`
-	Start_Hack = `Let's create a new branch from main to start working on the cleanup.
+	Start_Hack = `Let's create a new branch from %s to start working on the cleanup.
 This can be done with the following command from git-town:
 %s
 Type it out below to run it :)
 `
 	Start_Hack_Finished = `Well done!
-This command just created your new feature from the main branch you setup and checked it out.`
+This command just created your new feature branch from the %s branch you setup and checked it out.
+`
+
 	Start_Refactor_Changes = `Now make some changes to any file.
 Just something simple like creating a new file and writing "Refactoring", for example.
 Once you're finished, proceed with the guide.`
@@ -49,17 +52,69 @@ Once you're finished, proceed with the guide.`
 If the link didn't open in your default browser, manually open it to conclude the creation of the PR.
 
 And we've successfully put our first changes for review!`
-	Start_Bugfix = `We can now start working on the bugfix on top of these changes while they are still in review.
+
+	Start_Bugfix = `We can now start working on the bugfix on top of these changes while they are still in review, one of the advantages of the stacking workflow!
 To build on top of the previous changes, in the refactor branch still, we use the following git-town command:
 %s
 Give it a go:`
 	Start_Bugfix_Tree = `This is what the branch hierarchy is at the moment for git-town
-	<main>
+%s
   \
    1-refactor
      \
-      2-bugfix`
-	Start_Bugfix_Changes  = "Again, just make a small change just so that we can commit something and create a new PR."
-	Start_Bugfix_Finished = "Since git-town is aware that %s is a child of %s, it'll create the PR against the parent, %s\n"
+      2-bugfix
 
+`
+	Start_Bugfix_Changes  = "Again, just make a small change just so that we can commit something and create a new PR."
+	Start_Bugfix_Finished = "Since git-town is aware that %s is a child of %s, it'll create the PR against the parent, %s.\n"
+
+	Start_Feature = `
+Finally, we can begin working on our feature.
+Like the previous branch we want to create it on top of all of the changes (refactor and bugfix).
+
+You know the drill ;)
+%s`
+	Start_Feature_Tree = `The hierarchy now looks like this
+%s
+  \
+   1-refactor
+     \
+      2-bugfix
+        \
+         3-feature
+
+`
+	Start_Feature_Refactor         = "The feature branch was created successfully but we've got some comments on %s's PR. Let's apply them before working on the feature.\n"
+	Start_Feature_Refactor_Changes = "We've checked %s out. Create a new file and write \"Reviewer note\" inside.\n"
+	Start_Feature_Sync             = `With the changes made we can now go back to %s.
+But now we don't have the fixes that we just made.
+Luckily git-town facilitates synchronizing all our child branches, allowing us to avoid the tedious manual synchronization.
+This is achieved by running this simple command:
+%s
+
+What it'll do is go on each child branch (%s and %s) and merge the changes.
+`
+	Start_Feature_Changes  = "We're almost finished with making changes. Do it one more time for in this feature branch."
+	Start_Feature_Finished = "We've finished working on the feature so, like the previous two, we'll create a new PR."
+
+	Start_Ship = `
+We got the final approve for our refactor!
+
+Let's merge it into %s. We can do it with the %s command in git-town:
+%s
+`
+	Start_Finished = `And that's it!
+
+Stacking workflow allows you to separate PRs that adhere to the single responsability principle, leading to smaller PRs.
+It also allows you to work faster since you can continue chaining features while waiting for review/approval.
+
+This workflow can be achieved with just the commands of git, but git-town is a nice wrapper around git with some extra functionalities that make it easier to follow this workflow, like the %s command.
+
+You can find resources in the repository of this guide: %s
+
+Thank you for using this guide and I hope it helped you in understanding the stacking workflow and how to use git-town to that effect.
+At the moment the guide doesn't allow reruns :( since it's using hard coded values. If you want to run it again, just delete all the branches created and run again from start.
+
+Best of luck on your stacking :)
+`
 )
