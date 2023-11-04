8409003dc7674c773c0c20f7963478961bb8ea5a
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-04 13:19:40 +0000

    Git add changes & get num of changes funcs

---
+++ src/internal/git/changes.go
@@ -0,0 +1,30 @@
+package git
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/LoyalPotato/sg/src/internal/cli"
+	"github.com/LoyalPotato/sg/src/internal/messages"
+	"github.com/LoyalPotato/sg/src/internal/utils"
+)
+
+func GetNumOfChanges() (int, error) {
+	output, err := cli.RunCmdWithOutput("git", "status", "-s", "--porcelain")
+	if err != nil {
+		return 0, err
+	}
+
+	changes, err := utils.LineCounter(strings.NewReader(string(output)))
+
+	if err != nil {
+		return 0, err
+	}
+
+	return changes, nil
+}
+
+func AddAllChanges() error {
+	fmt.Println(messages.Git_Add_All)
+	return cli.RunCmd("git", "add", "-A")
+}
