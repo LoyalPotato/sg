d791e15c3f7e698e50b7a3b4d658639cddec4eb6
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-04 13:17:41 +0000

    Add error msgs

--- src/internal/messages/errors.go
+++ src/internal/messages/errors.go
@@ -1,5 +1,8 @@
 package messages
 
 const (
-	Empty_String = "please provide a value"
+	Empty_String  = "please provide a value"
+	Generic_Error = "error has occurred %v\n"
+
+	Error_Git_Commit = "couldn't complete commit %v\n"
 )
