e328ca7fb47efa2d05e6ee11f2d2d48e42c88ec4
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-04 13:17:32 +0000

    Add line counter util

--- src/internal/utils/strings.go
+++ src/internal/utils/strings.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"bytes"
+	"io"
 	"strings"
 	"unicode"
 )
@@ -18,4 +20,26 @@ func RemoveNonPrintables(s string) string {
 		}
 		return -1
 	}, s)
+}
+
+// Count number of newlines in reader
+//
+// Credits: https://stackoverflow.com/a/24563853/6251097
+func LineCounter(r io.Reader) (int, error) {
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+
+		switch {
+		case err == io.EOF:
+			return count, nil
+
+		case err != nil:
+			return count, err
+		}
+	}
 }
