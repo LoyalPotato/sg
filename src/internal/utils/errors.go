5cf156c2bc0e20f3477ea6a40865e5fd525bedd0
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-04 18:32:20 +0000

    Add exit code for interrupt

--- src/internal/utils/errors.go
+++ src/internal/utils/errors.go
@@ -6,6 +6,9 @@ import (
 	"os/exec"
 )
 
+// Find more about exit codes here: https://tldp.org/LDP/abs/html/exitcodes.html
+const EXIT_CODE_INTERRUPT int = 130
+
 func GetExitCode(err error) int {
 	if exitError, ok := err.(*exec.ExitError); ok {
 		return exitError.ExitCode()
