52cb21f43325d2b3d71db66460dcb1e1c697c48c
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-11-04 13:17:57 +0000

    Wrapper around git town cmd

---
+++ src/internal/git-town/root.go
@@ -0,0 +1,13 @@
+package gittown
+
+import (
+	"os"
+
+	"github.com/LoyalPotato/sg/src/internal/cli"
+)
+
+const gittownRoot = "git-town"
+
+func runGitTown(args ...string) error {
+	return cli.RunCmdCustomStdOut(gittownRoot, os.Stdout, nil, args...)
+}
